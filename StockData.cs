ee623f2286dc8c70724238cd5b98e0eedad0b509
Chengxin.Cai <chengxin.cai@gmail.com>
2016-01-05 17:11:01 +0800

    more

---
+++ StockData.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.IO;
+
+namespace Stock_fund
+{
+    public class StockData
+    {
+        public string Code { get; set; }
+        public double Price { get; set; }
+        public double Last { get; set; }
+        public double Open { get; set; }
+        public double High { get; set; }
+        public double Low { get; set; }
+        public double Volume { get; set; }
+        public double Amount { get; set; }
+        public double CircuValue { get; set; }
+        public double Value { get; set; }
+
+        public static StockData GetData(string code)
+        {
+            string URL = "http://qt.gtimg.cn/q=";
+            if (code.StartsWith("60"))
+                URL += "sh";
+            else
+                URL += "sz";
+
+            URL += code;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            WebResponse resp = request.GetResponse();
+            Stream webStream = resp.GetResponseStream();
+            StreamReader reader = new StreamReader(webStream);
+
+            string response = reader.ReadToEnd();
+            Regex rx = new Regex("(.+)=\"(.+)\"");
+
+            string line = response.Split(new Char[]{'\n'})[0];
+
+            Match m = rx.Match(line);
+            if (m.Success)
+            {
+                string[] items = m.Groups[2].ToString().Split(new char[]{'~'});
+                return new StockData()
+                {
+                    Code=code,
+                    Price=Double.Parse(items[3]),
+                    Last=Double.Parse(items[4]),
+                    Open=Double.Parse(items[5]),
+                    High=Double.Parse(items[33]),
+                    Low=Double.Parse(items[34]),
+                    Amount = Double.Parse(items[36]),
+                    Volume=Double.Parse(items[37]),
+                    CircuValue=Double.Parse(items[44]),
+                    Value=Double.Parse(items[45]),
+                };
+            }
+
+            return null;
+        }
+
+       
+    }
+}
---
+++ StockDetailWin.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Stock_fund
+{
+    /// <summary>
+    /// Interaction logic for StockDetails.xaml
+    /// </summary>
+    public partial class StockDetailWin : Window
+    {
+        class Node
+        {
+            public string Name { get; set; }
+            public string Value { get; set; }
+            public List<Node> Nodes { get; set; }
+
+            public Node()
+            {
+                Nodes = new List<Node>();
+            }
+
+            public override string ToString()
+            {
+                return Name + ":" + Value;
+            }
+        }
+
+        public StockDetailWin()
+        {
+            InitializeComponent();
+        }
+
+        public void SetDataSource(StockData data)
+        {
+
+            Node rootNode = new Node {Name="Code", Value=data.Code};
+            rootNode.Nodes.Add(new Node {Name="Price", Value=data.Price.ToString()});
+            rootNode.Nodes.Add(new Node {Name="Open", Value=data.Open.ToString()});
+            rootNode.Nodes.Add(new Node {Name="Last", Value=data.Last.ToString()});
+
+            List<Node> nodes = new List<Node>(){rootNode};
+            tvStockDetail.ItemsSource = nodes;
+        }
+
+    }
+}
